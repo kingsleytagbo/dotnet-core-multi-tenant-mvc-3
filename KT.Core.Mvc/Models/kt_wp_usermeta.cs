9e8661250eab9019dcc507d325958f172545d2d0
ktagbo <kingsleytagbo@gmail.com>
2020-08-13 11:23:15 -0500

    features/development

---
+++ KT.Core.Mvc/Models/kt_wp_usermeta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace KT.Core.Mvc.Models
+{
+
+    public class kt_wp_usermeta
+    {
+        public UInt64 umeta_id { get; set; }
+        public UInt64 user_id { get; set; }
+        public string meta_key { get; set; }
+        public string meta_value { get; set; }
+    }
+}
