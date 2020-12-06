d994cdbea608ff7fbd0e7dce3d7473215a32abb3
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-12-06 17:01:07 -0600

    sql SERVER

--- KT.Core.Mvc/Models/kt_wp_post.cs
+++ KT.Core.Mvc/Models/kt_wp_post.cs
@@ -30,5 +30,6 @@ namespace KT.Core.Mvc.Models
         public string post_type { get; set; }
         public string post_mime_type { get; set; }
         public Int64 comment_count { get; set; }
+        public UInt64 site_id { get; set; }
     }
 }
