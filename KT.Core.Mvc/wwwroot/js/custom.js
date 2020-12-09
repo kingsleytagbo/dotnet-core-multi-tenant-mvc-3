fa05307891361146351b7fef0daeb43231c210d8
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-12-09 08:02:20 -0600

    auth headers

---
+++ KT.Core.Mvc/wwwroot/js/custom.js
@@ -0,0 +1,9 @@
+﻿const SiteModule = (function (Page) {
+    return {
+    };
+}(PageModule));
+
+ $(document).ready(function () {
+        console.log(SiteModule)
+     //SiteModule.loadCategories();
+ });
