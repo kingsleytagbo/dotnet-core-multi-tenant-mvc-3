4dce665c4dbe8ab4183feba34b0e3167e88ccac0
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-12-10 20:49:25 -0600

    auth headers

---
+++ KT.Core.Mvc/Models/wp_image_file.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KT.Core.Mvc.Models
+{
+    public class wp_image_file: wp_image
+    {
+        public IFormFile FormFile { get; set; }
+    }
+}
